0ce2788700b2ceacfc4c9cfa4a82fc96bc1b33fc
vdubey <vijay_dubey@cable.comcast.com>
2017-04-26 21:13:00 -0400

    Performance telemetry marker
    Reason for change: Performance telemetry marker
    Test Procedure: test Performance telemetry
    Risks: Low
    Signed-off-by: vdubey <vijay_dubey@cable.comcast.com>
    
    Change-Id: I4600f54dc7b1ee5a1c730034058703e8c4e01ab7

---
+++ src/dcamem.c
@@ -0,0 +1,125 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2016 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+
+
+/**
+* @defgroup dca
+* @{
+**/
+
+
+
+
+/**
+* @defgroup dca
+* @{
+* @defgroup src
+* @{
+**/
+
+//cpu and free memory
+#include <stdio.h>
+
+#define MAXLEN 512
+
+int getMemoryUsage(char *memoryUtilization);
+
+int main()
+{
+
+	 char memoryUtilization[MAXLEN]={'\0'};
+
+	 if(getMemoryUsage(memoryUtilization))
+	 {
+		 printf("USED_MEM:%s\n", memoryUtilization);
+	 }
+	 else
+	 {
+		 printf("USED_MEM:NOT AVAILABLE\n");
+	 }
+	 return 0;
+}
+
+
+/** @description: To get memory usage of the device.
+ *  @param memoryUtil memoryusage of device.
+ *  @return 1 on success.
+ */
+
+int getMemoryUsage(char *memoryUtilization)
+{
+	FILE *memoryinfo;
+	char line[MAXLEN];
+	char tmp[MAXLEN];
+	long long  memTotal = 0;
+	long long  memFree = 0;
+	long long  memoryInUse=0;
+
+
+	/* Open /proc/cpuinfo file*/
+	if ((memoryinfo = fopen("/proc/meminfo", "r")) == NULL)
+	{
+		printf("Failed to get Memory Utilization mode\n");
+		return 0;
+	}
+
+	/* Search until the "MemTotal" entry is found*/
+	while(fgets(line, MAXLEN, memoryinfo))
+	{
+		sscanf(line, "%s", tmp);
+		if((strcmp(tmp,"MemTotal:") == 0))
+		{
+			sscanf(line, "%*s %s", tmp);
+			memTotal = atoll(tmp);
+			break;
+		}
+	}
+
+	/* Search until the "MemFree" entry is found*/
+	while(fgets(line, MAXLEN, memoryinfo))
+	{
+		sscanf(line, "%s", tmp);
+		if((strcmp(tmp,"MemFree:") == 0))
+		{
+			sscanf(line, "%*s %s", tmp);
+			memFree = atoll(tmp);
+			break;
+		}
+	}
+
+	fclose(memoryinfo);
+	memoryInUse = (memTotal - memFree);
+	if(memoryUtilization)
+	{
+		sprintf(memoryUtilization,"%lld",memoryInUse);
+	}
+	else
+	{
+		printf("Exit from get Memory Utilization due to NULL pointer");
+		return 0;
+	}
+	return 1;
+}
+
+/** @} */
+
+
+/** @} */
+/** @} */
