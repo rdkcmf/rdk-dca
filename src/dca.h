e241ef6bf2a86a2ed971d6605e96480a568cbeba
skakko494 <Shibu_KV@cable.comcast.com>
2019-12-05 16:01:25 -0800

    BLDK-693 : Correcting errors with copyrights
    Reason for change: Errors or missing copyrights
    Test Procedure: Build and verify
    Risks: None
    Signed-off-by: skakko494 <Shibu_KV@cable.comcast.com>
    
    (cherry picked from commit 4455a80efc9372d249f7d9661018b9767a925dfb)
    
    Change-Id: Ieb2fe583628e17c01372ca1928aa149cb2198a93

--- src/dca.h
+++ src/dca.h
@@ -1,10 +1,21 @@
 /*
- * dca.h
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
  *
- *  Created on: Aug 11, 2019
- *      Author: skv
- */
-
+ * Copyright 2019 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 #ifndef SRC_DCA_H_
 #define SRC_DCA_H_
 #include <cjson/cJSON.h>
